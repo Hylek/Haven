df198460c52d320b60d0948f730a1ce8b83d5af8
Daniel Cumbor <dcdolphy@gmail.com>
2020-09-30 20:33:44 +0100

    Added base project
    -- Added base Unity project
    -- Added basic test scene layout
    -- Created first GOAP script (Not started)

---
+++ Assets/Scripts/GOAP/GOAP_World.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Daniel Cumbor in 2020. Thanks Covid-19!
+
+public class GOAP_World : MonoBehaviour
+{
+
+}
