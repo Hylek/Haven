d9ba745868b4e84faaf2ac061d1afec9ab7b78d6
Daniel Cumbor <dcdolphy@gmail.com>
2020-12-04 19:01:55 +0000

    Began work on GOAP project. Implemented world and world states classes.

--- Assets/Scripts/GOAP/GOAP_World.cs
+++ Assets/Scripts/GOAP/GOAP_World.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 // Written by Daniel Cumbor in 2020. Thanks Covid-19!
 
-public class GOAP_World : MonoBehaviour
+// Singleton for the world and it's states.
+public sealed class GOAP_World : MonoBehaviour
 {
+    private static readonly GOAP_World singleton = new GOAP_World();
+    private static GOAP_World_States worldStates;
 
+    static GOAP_World()
+    {
+        worldStates = new GOAP_World_States();
+    }
+
+    public GOAP_World_States getWorldStates()
+    {
+        return worldStates;
+    }
+
+    public static GOAP_World getWorldInstance()
+    {
+        return singleton;
+    }
 }
---
+++ Assets/Scripts/GOAP/GOAP_World_States.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Daniel Cumbor in 2020. Thanks Covid-19!
+
+[System.Serializable]
+public class WORLD_STATE
+{
+    public string key;
+    public int value;
+}
+
+// Helper logic for handling GOAP's world states.
+public class GOAP_World_States
+{
+    private Dictionary<string, int> states;
+
+    public GOAP_World_States()
+    {
+        states = new Dictionary<string, int>();
+    }
+
+    public Dictionary<string, int> getStates()
+    {
+        return states;
+    }
+
+    // Check if a state exists
+    protected bool hasState(string key)
+    {
+        return states.ContainsKey(key);
+    }
+
+    // Checks if a state exists, if it does modify, if not add it.
+    public void setState(string key, int value)
+    {
+        if(hasState(key))
+        {
+            states[key] = value;
+        }
+        else
+        {
+            states.Add(key, value);
+        }
+    }
+
+    public void editState(string key, int value)
+    {
+        if(hasState(key))
+        {
+            states[key] += value;
+            if(states[key] <= 0)
+            {
+                deleteState(key);
+            }
+        }
+        else
+        {
+            setState(key, value);
+        }
+    }
+
+    public void deleteState(string key)
+    {
+        if(hasState(key))
+        {
+            states.Remove(key);
+        }
+        else
+        {
+            Debug.Log("CAUTION: Cannot delete state, it does not exist!");
+        }
+    }
+}
